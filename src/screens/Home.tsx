8cae6936d99a3acf75c9ddda16512c7e0e194936
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-06-29 22:42:31 +0530

    fix(home): make mobile and tablet responsive

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,13 +4,13 @@ export default function Home() {
 
     return (
         <>
-            <section className='h-full pl-20 pt-15 grid grid-cols-[2fr_1fr] items-center flex justify-start items-center'>
+            <section className='h-full pl-20 max-md:pl-10 pt-15 grid grid-cols-[2fr_1fr] items-center flex justify-start items-center'>
 
                 <div className="flex flex-col items-start justify-center">
 
                     <span className='font-medium mb-8'>4+ years of work experience · Product Design</span>
 
-                    <span className="text-[48px] text-black gap-4 leading-tight">
+                    <span className="text-[48px] max-sm:text-[36px] text-black gap-4 leading-tight">
                         Hey! I'm <span className='text-[#8A93EF]'>Kashika.</span>
                         <br />
                         I enjoy crafting user centric,
@@ -28,9 +28,9 @@ export default function Home() {
                 </div>
             </section>
 
-            <hr className="w-96 ml-20 mt-10 h-[1.0px] bg-gradient-to-r from-transparent via-slate-400 to-transparent border-0" />
+            <hr className="w-96 ml-20 max-md:ml-10 mt-10 h-[1.0px] bg-gradient-to-r from-transparent via-slate-400 to-transparent border-0" />
 
-            <section className='pl-20 pt-10 grid grid-cols-2 items-center'>
+            <section className='pl-20 max-md:pl-10 pt-10 grid grid-cols-2 items-center'>
                 <div className="flex md:flex-row items-start justify-center max-sm:flex-col max-sm:gap-10 md:gap-30">
                     <div className='flex flex-col items-start justify-center'>
                         <span className='text-2xl text-semibold'>Currently</span>
